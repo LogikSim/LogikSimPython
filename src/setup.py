4fdc44ecb5d5dd93cc006e1f7998da053694a1c8
Stefan Hacker <dd0t@users.sourceforge.net>
2014-10-15 20:01:29 +0200

    Add "generate" command to setup.py.
    It allows running UIC on all .ui files in the project
    and RCC on all .qrc files in the project. The files are
    found by searching them recursively by file-ending. The
    target file-names follows the expected PySide conventions.
    Namely "ui_<filename>.py" for .ui files and "<filename>_rc.py"
    for .qrc files.

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,11 @@ Script to build LogikSim binaries with cx_Freeze for Windows.
 '''
 
 import sys
+import os
+import fnmatch
+import subprocess
 from cx_Freeze import setup, Executable
+from distutils.core import Command
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -25,6 +29,56 @@ exe = Executable(
         base = base,
 )
 
+class GenerateCommand(Command):
+    description = "Generates .py files from Qt .ui and .qrc files"
+    user_options = [
+        ('no-uic', None, 'Disables UIC'),
+        ('no-rcc', None, 'Disables RCC')
+    ]
+
+    def initialize_options(self):
+        self.no_uic = False
+        self.no_rcc = False
+        
+    def finalize_options(self):
+        self.no_uic = bool(self.no_uic)
+        self.no_rcc = bool(self.no_rcc)
+        self.dry_run = bool(self.dry_run) # Global option
+
+    def recursive_search(self, filter, search_directory = '.'):
+        """
+        Recursively searches for files matching the glob filter
+        and returns them as a list of directory, filename tuples.
+        """
+        found = []
+        for root, directories, files in os.walk(search_directory):
+            for file in fnmatch.filter(files, filter):
+                found.append((root, file))
+
+        return found
+
+    def run(self):
+        if self.dry_run:
+            call = lambda params, cwd = None: print("In '{0}' would run: {1}".format(cwd, " ".join(params)))
+        else:
+            call = subprocess.check_call
+
+        if not self.no_uic:
+            print("Running uic...")
+            for (root, source_file) in self.recursive_search("*.ui"):
+                target_file = "ui_{0}.py".format(os.path.splitext(source_file)[0])
+                print("  " + os.path.join(root, source_file))
+                call(["pyside-uic", source_file, "-o", target_file], cwd = root)
+            print("Done")
+
+        if not self.no_rcc:
+            print("Running rcc...")
+            for (root, source_file) in self.recursive_search("*.qrc"):
+                target_file = "{0}_rc.py".format(os.path.splitext(source_file)[0])
+                print("  " + os.path.join(root, source_file))
+                call(["pyside-rcc", "-py3", source_file, "-o", target_file], cwd = root)
+            print("Done")
+
 setup(
         name = "QCanvasTest",
         version = "0.0.1",
@@ -32,6 +86,7 @@ setup(
         options = {"build_exe": {"compressed": True,
                                  "includes" : ["PySide.QtXml"],
                                  "include_files" : ["../LICENSE.txt",
-                                                    "../AUTHORS.txt"]}}, 
+                                                    "../AUTHORS.txt"]}},
+        cmdclass = {"generate": GenerateCommand },
         executables = [exe]
 )
