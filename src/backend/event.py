4db26b66a72fe65c57ab402ac2cfc614298e90c8
Stefan Hacker <dd0t@users.sourceforge.net>
2014-12-14 19:15:39 +0100

    Introduce total event ordering to prevent event racing issues.
    Without total event ordering we cannot properly
    handle things occurring at the same time at the moment.
    
    We can probably solve this another way but with the current
    architecture if we have two edges on different inputs on the
    same element at the same time we can't apply them without
    scheduling an independent output event for each edge. This
    isn't a problem as long as our output events stay ordered
    correctly. If they don't we might end up in an invalid state.

--- src/backend/event.py
+++ src/backend/event.py
@@ -8,6 +8,8 @@
 
 
 class Event(object):
+    sequence_counter = 0
+
     def __init__(self, when, process):
         """
         Creates a new event scheduled at a given time.
@@ -20,6 +22,17 @@ class Event(object):
         self.when = when
         self.process = process
 
+        self.sequence = Event.sequence_counter
+        # FIXME: Kinda ugly but we need them to be executed in the order
+        #  they were created or we might end up having events occurring at
+        #  the same time racing each other leading to invalid outputs
+        #  depending on execution order. E.g. if we have an Or element and
+        #  a (1,0)->1 is scheduled at the same time as a (0,0)->0
+        #  the constant output delay might mean we might output the edge to 0
+        #  first ending up in an invalid 1 output state after both edges.
+        #
+        Event.sequence_counter += 1
+
     def __str__(self):
         return "Event({0},{1})".format(self.when, self.process)
 
@@ -27,4 +40,5 @@ class Event(object):
         """
         :return: True if `other` is scheduled after this event.
         """
-        return self.when < other.when
+        return self.when < other.when if self.when != other.when else \
+            self.sequence < other.sequence
