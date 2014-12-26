cc8f677e7388843f6cd1f9c337796134fb41b165
Christian Brugger <brugger.chr@gmail.com>
2014-12-26 05:07:05 +0100

    added visual tests for inserting lines

--- src/tests/test_inserting.py
+++ src/tests/test_inserting.py
@@ -76,9 +76,16 @@ class TestLineRoute(unittest.TestCase):
         line_route.route()
         line_route.do_temp_insert()
 
-        # check line count
+        # check line count & temp
         trees = [item for item in scene.items() if isinstance(item, LineTree)]
         self.assertEqual(len(trees), 1)
+        self.assertTrue(trees[0].is_temporary())
+
+        # insert & check line count & temp
+        line_route.do_insert()
+        trees = [item for item in scene.items() if isinstance(item, LineTree)]
+        self.assertEqual(len(trees), 1)
+        self.assertFalse(trees[0].is_temporary())
 
         # check edges
         tree = trees[0]
@@ -95,3 +102,272 @@ class TestLineRoute(unittest.TestCase):
                                                           tsp(5, 9))))
         self.assertFalse(tree.contains_line(QtCore.QLineF(tsp(5, 10),
                                                           tsp(5, 11))))
+
+
+class TestLineRouteGraphical(unittest.TestCase):
+    def fill_scene(self, scene, input_area):
+        lines = input_area.split('\n')
+
+        def add_tree(p1, p2):
+            scene.addItem(LineTree([scene.to_scene_point(p1),
+                                    scene.to_scene_point(p2)]))
+
+        # insert line stubs
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                if char in '-+┬┴':  # horizontal
+                    add_tree((x - 0.5, y), (x + 0.5, y))
+                if char in '┤├|+':
+                    add_tree((x, y - 0.5), (x, y + 0.5))
+                if char in '┐˄┬┌x':
+                    add_tree((x, y), (x, y + 0.5))
+                if char in '┘└˅┴x':
+                    add_tree((x, y), (x, y - 0.5))
+                if char in '┌└<├x':
+                    add_tree((x, y), (x + 0.5, y))
+                if char in '>┤┐┘x':
+                    add_tree((x, y), (x - 0.5, y))
+
+        # merge trees
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                for p in [(x, y), (x + 0.5, y), (x, y + 0.5)]:
+                # for p in [(x, y)]:
+                    pivot = scene.to_scene_point(p)
+                    items = scene.items(pivot)
+                    if len(items) > 1:
+                        tree = items[0]
+                        for item in items[1:]:
+                            tree.merge_tree(item)
+                            scene.removeItem(item)
+
+    def to_positions(self, scene, position_area):
+        scene = GridScene()
+        lines = position_area.split('\n')
+
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                if char in 'AX':
+                    point_a = x, y
+                if char in 'BX':
+                    point_b = x, y
+
+        return (scene.to_scene_point(point_a),
+                scene.to_scene_point(point_b))
+
+    def verify_output_data(self, scene, output_area):
+        lines = output_area.split('\n')
+
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                pivot = scene.to_scene_point((x, y))
+                items = scene.items(pivot)
+                if char in 'x┴˄˅<┤├>┬-|┘┐┌└':
+                    self.assertEqual(len(items), 1)
+                    self.assertIsInstance(items[0], LineTree)
+                    self.assertEqual(items[0].is_edge(pivot),
+                                     char in 'x┴˄˅<┤├>┬')
+                elif char in ['+']:
+                    self.assertEqual(len(items), 2)
+                    for item in items:
+                        self.assertIsInstance(item, LineTree)
+                        self.assertEqual(item.is_edge(pivot), False)
+
+    def visual_test(self, input_area, position_area, output_area,
+                    view_result=False):
+        """
+        Visual test case definition for inserting lines.
+
+        Visual test cases are defined based on tree areas, basically
+        strings defined as a block representing a two-dimensional grid:
+
+        :param input_area: define lines for the starting scene
+        :param position_area: define start and endpoint for inserting
+        :param output_area: define output lines
+
+        Lines are encoded with unicode characters: ┴˄˅<┤├>┬-|┘┐┌└:
+            - horizontal peace, | vertical mid peace
+            < left end, > right end, ˄ upper end, ˅ lower end
+            + crossing lines, x intersecting lines
+            ┤┬ ┘┐┌└ ┴├, corners
+
+        Positions are given as A, B and X:
+            A start, B end, X start and end
+
+        When specifying the areas it is advisable to copy the input area,
+        to the position area, then adding positions, then copy the result
+        to the ouput area and add the added line.
+
+        :param view_result: open scene with resulting grid for debugging
+
+        """
+        scene = GridScene()
+        self.fill_scene(scene, input_area)
+        start, end = self.to_positions(scene, position_area)
+        line_route = LineRouteBetweenPoints(scene, start, end)
+        line_route.route()
+        line_route.do_insert()
+
+        if view_result:
+            import schematics.mouse_modes
+            import sys
+            from PySide import QtGui
+            app = QtGui.QApplication.instance()
+            if app is None:
+                app = QtGui.QApplication(sys.argv)
+            view = schematics.EditSchematicView()
+            view.setScene(scene)
+            view.show()
+            view.ensureVisible(QtCore.QRectF(0, 0, 0, 0))
+            view.setMouseMode(schematics.mouse_modes.InsertLineMode)
+            app.exec_()
+
+        self.verify_output_data(scene, output_area)
+
+    def test_horizontal_line(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└
+        input_area = """
+
+
+
+        """
+        position_area = """
+
+           A             B
+
+        """
+        output_area = """
+
+           <------------->
+
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    def test_simple_split_line(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└
+        input_area = """
+           <----------->
+        """
+        position_area = """
+           <----A------>
+
+
+
+                B
+        """
+        output_area = """
+           <----┬------>
+                |
+                |
+                |
+                ˅
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    def test_simple_crossover_line(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└
+        input_area = """
+
+           <----------->
+
+        """
+        position_area = """
+                 A
+           <----------->
+                 B
+        """
+        output_area = """
+                 ˄
+           <-----+----->
+                 ˅
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    def test_simple_crossover_two_lines(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└
+        input_area = """
+
+           <----------->
+           <----------->
+        """
+        position_area = """"
+                A
+           <----------->
+           <----B------>
+        """
+        output_area = """"
+                ˄
+           <----+------>
+           <----┴------>
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    @unittest.skip("Broken")
+    def test_simple_connected_crossover_two_lines(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└
+        input_area = """
+
+           ┌----------->
+           └----------->
+        """
+        position_area = """"
+                A
+           ┌----------->
+           └----B------>
+        """
+        output_area = """"
+                ˄
+           ┌----+------>
+           └----┴------>
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    def test_simple_connecting_two_lines(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└|
+        input_area = """
+           ˄        ˄
+           |        |
+           |        |
+           |        |
+           ˅        ˅
+        """
+        position_area = """
+           ˄        ˄
+           |        |
+           A        B
+           |        |
+           ˅        ˅
+        """
+        output_area = """
+           ˄        ˄
+           |        |
+           ├--------┤
+           |        |
+           ˅        ˅
+        """
+        self.visual_test(input_area, position_area, output_area)
+
+    def test_extend_over_corner(self):
+        # ┴˄˅<┤├>┬-|┘┐┌└|
+        input_area = """
+           <--------┐
+                    |
+                    |
+                    |
+                    ˅
+        """
+        position_area = """
+           <---A----┐    B
+                    |
+                    |
+                    |
+                    ˅
+        """
+        output_area = """
+           <--------┬---->
+                    |
+                    |
+                    |
+                    ˅
+        """
+        self.visual_test(input_area, position_area, output_area)
