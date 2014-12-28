dbeea82f0b1fc027cf92ddd2ddbc20a5bfe11873
Christian Brugger <brugger.chr@gmail.com>
2014-12-28 08:52:33 +0100

    Added selections and cursor changes to Library

--- src/schematics/library_view.py
+++ src/schematics/library_view.py
@@ -14,8 +14,49 @@ import collections
 
 from PySide import QtGui, QtCore
 
+import logicitems
 import schematics
 from symbols import AndItem
+
+
+class TileItem(QtGui.QGraphicsRectItem):
+    def __init__(self, *args, overlapp=0, **kargs):
+        super().__init__(*args, **kargs)
+
+        # set rect
+        new_rect = self.rect().adjusted(-overlapp, -overlapp,
+                                        2 * overlapp, 2 * overlapp)
+        self.setRect(new_rect)
+
+        self.setZValue(1)
+        # self.setFlag(QtGui.QGraphicsItem.ItemIsSelectable)
+
+        self._is_selected = False
+        self._is_hover = False
+
+        self._update_state()
+
+    def _update_state(self):
+        if self._is_selected:
+            self.setVisible(True)
+            self.setPen(QtGui.QPen(QtGui.QColor(40, 125, 210)))
+            self.setBrush(QtGui.QBrush(QtGui.QColor(40, 125, 210, 100)))
+        elif self._is_hover:
+            self.setVisible(True)
+            self.setPen(QtGui.QPen(QtGui.QColor(40, 125, 210)))
+            self.setBrush(QtGui.QBrush(QtGui.QColor(40, 125, 210, 60)))
+        else:
+            self.setVisible(False)
+        self.update()
+
+    def setHover(self, value):
+        self._is_hover = value
+        self._update_state()
+
+    def setSelected(self, value):
+        super().setSelected(value)
+        self._is_selected = value
+        self._update_state()
 
 
 class ItemListScene(schematics.GridScene):
@@ -25,6 +66,8 @@ class ItemListScene(schematics.GridScene):
     _tile_margin = 0.5
     # tile spacing in grid points
     _tile_spacing = 1
+    # selection overlap in grid points
+    _tile_selection_overlapp = 0.15
 
     def __init__(self, *args, **kargs):
         super(ItemListScene, self).__init__(*args, **kargs)
@@ -45,6 +88,10 @@ class ItemListScene(schematics.GridScene):
         self._items = []
         self._col_width = {}
         self._row_height = {}
+        self._tiles = []
+        # store states
+        self._selected_tile = None
+        self._hovered_tile = None
 
     def roundToGrid(self, pos, y=None):
         if y is not None:
@@ -64,19 +111,19 @@ class ItemListScene(schematics.GridScene):
     def _rebuild_layout(self):
         if self._col_count is None and self._row_count is None:
             return
-
         if self._layout_horizont:
             self._col_count = math.ceil(len(self._items) / self._row_count)
 
+        # create layout
         layout = QtGui.QGraphicsGridLayout()
         self._top_widget.setLayout(layout)  # widget takes ownership of layout
         grid = self.get_grid_spacing()
         layout.setSpacing(grid * self._tile_spacing)
 
+        # add items
+        next_index = 0, 0
         self._col_width = collections.defaultdict(int)
         self._row_height = collections.defaultdict(int)
-
-        next_index = 0, 0
         for item in self._items:
             row, col = next_index
 
@@ -90,9 +137,46 @@ class ItemListScene(schematics.GridScene):
             col += 1
             next_index = row + col // self._col_count, col % self._col_count
 
+        # update scene rect
         self._top_widget.updateGeometry()
         self.setSceneRect(QtCore.QRectF(self._top_widget.pos(),
                                         self._top_widget.size()))
+
+        self._rebuild_tiles()
+
+    def _rebuild_tiles(self):
+        # for now we asume that items are only added
+        if self._selected_tile is not None:
+            sel_index = self._tiles.index(self._selected_tile)
+        else:
+            sel_index = None
+
+        # remove all old tiles
+        for tile in self._tiles:
+            self.removeItem(tile)
+        self._tiles = []
+
+        grid = self.get_grid_spacing()
+        row_start = self._tile_margin
+        for row in self._row_height:
+            col_start = self._tile_margin
+            for col in self._col_width:
+                tile = TileItem(col_start * grid,
+                                row_start * grid,
+                                self._col_width[col] * grid,
+                                self._row_height[row] * grid,
+                                overlapp=self._tile_selection_overlapp * grid)
+                self.addItem(tile)
+                self._tiles.append(tile)
+                if len(self._tiles) == len(self._items):
+                    break
+                col_start += self._col_width[col] + self._tile_spacing
+            row_start += self._row_height[row] + self._tile_spacing
+
+        if sel_index is not None:
+            self._tiles[sel_index].setSelected(True)
+            self._selected_tile = self._tiles[sel_index]
+            self._hovered_tile = None
 
     def add_item(self, item_class):
         # add item to scene
@@ -109,7 +193,7 @@ class ItemListScene(schematics.GridScene):
 
         self._rebuild_layout()
 
-    def get_item_at_pos(self, pos):
+    def _index_at_pos(self, pos):
         """Returns item in tile or None at given position."""
         gpos = pos / self.get_grid_spacing()
 
@@ -125,10 +209,44 @@ class ItemListScene(schematics.GridScene):
 
         if row is None or col is None:
             return None
+        return row * self._col_count + col
+
+    def get_item_at_pos(self, pos):
+        """Returns item in tile or None at given position."""
+        index = self._index_at_pos(pos)
         try:
-            return self._items[row * self._col_count + col]
+            if index is not None:
+                return self._items[index]
         except IndexError:
             return None
+
+    def _get_tile_at_pos(self, pos):
+        index = self._index_at_pos(pos)
+        try:
+            if index is not None:
+                return self._tiles[index]
+        except IndexError:
+            return None
+
+    def select_item(self, pos):
+        if self._selected_tile is not None:
+            self._selected_tile.setSelected(False)
+            self._selected_tile = None
+
+        tile = self._get_tile_at_pos(pos)
+        if tile is not None:
+            tile.setSelected(True)
+            self._selected_tile = tile
+
+    def hover_item(self, pos):
+        if self._hovered_tile is not None:
+            self._hovered_tile.setHover(False)
+            self._hovered_tile = None
+
+        tile = self._get_tile_at_pos(pos)
+        if tile is not None:
+            tile.setHover(True)
+            self._hovered_tile = tile
 
 
 class LibraryView(schematics.GridView):
@@ -139,18 +257,40 @@ class LibraryView(schematics.GridView):
         self.setTransformationAnchor(QtGui.QGraphicsView.NoAnchor)
 
         self.setInteractive(False)
-        # self.setAcceptDrops(True)
+        self.setAcceptDrops(True)
 
         self._layout_horizont = False
+        self._drag_start_pos = None
 
-        for _ in range(100):
+        for _ in range(10):
             self.scene().add_item(AndItem)  # TestRect)
 
     def mousePressEvent(self, event):
-        # get item in tile
-        item = self.scene().get_item_at_pos(self.mapToScene(event.pos()))
+        if event.button() == QtCore.Qt.LeftButton:
+            self._drag_start_pos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        gpos = self.mapToScene(event.pos())
+
+        self.scene().hover_item(gpos)
+        if self.scene().get_item_at_pos(gpos) is not None:
+            self.setCursor(QtCore.Qt.SizeAllCursor)
+        else:
+            self.setCursor(QtCore.Qt.ArrowCursor)
+
+        if not event.buttons() & QtCore.Qt.LeftButton:
+            return
+
+        if (event.pos() - self._drag_start_pos).manhattanLength() < \
+                QtGui.QApplication.startDragDistance():
+            return
+
+        # get item in tile & start drag&drop
+        item = self.scene().get_item_at_pos(gpos)
         if item is None:
             return
+
+        self.scene().select_item(gpos)
 
         mimeData = QtCore.QMimeData()
         mimeData.setData('application/x-components', str(item._input_count))
@@ -159,6 +299,15 @@ class LibraryView(schematics.GridView):
         drag.setMimeData(mimeData)
 
         drag.exec_()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.scene().select_item(self.mapToScene(event.pos()))
+
+    def dragEnterEvent(self, event):
+        # accept them to show nice cursor
+        if event.mimeData().hasFormat('application/x-components'):
+            event.accept()
 
     def wheelEvent(self, event):
         if self._layout_horizont:
@@ -183,8 +332,6 @@ class LibraryView(schematics.GridView):
         return QtCore.QSize(full_width, full_height)
 
     def resizeEvent(self, event):
-        print(event.size().height(), self.size().height(),
-              self.get_full_size(event).height())
         full_size = self.get_full_size(event)
         self._layout_horizont = full_size.width() > full_size.height()
         if self._layout_horizont:
@@ -259,4 +406,3 @@ class LibraryView(schematics.GridView):
 
         rel_scale = new_scale / self.getAbsoluteScale()
         self.scale(rel_scale, rel_scale)
-
